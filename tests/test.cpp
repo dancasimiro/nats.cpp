9bde91b38ad2971dd0b85ebae214c5610bf4aa25
Daniel C. Casimiro <dan.casimiro@gmail.com>
2025-03-03 11:30:37 -0500

    make libraries to enable testing

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,12 +1,8 @@
+#include "nats_client.h"
+
 #include <catch2/catch_test_macros.hpp>
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
-}
-
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+TEST_CASE( "Link NATS Client", "[link]" ) {
+    net::io_context io_context;
+    NATSClient nats_client(io_context, "demo.nats.io", "4222");
 }
